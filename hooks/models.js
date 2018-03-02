2fa3d76d583f7038a8376d1ce6e63cd5447d0a3a
Muhammad Dadu <muhammad@dadu.co>
2018-03-02 09:39:49 +0000

    Improved code and fix bug

--- hooks/models.js
+++ hooks/models.js
@@ -3,24 +3,24 @@
  * Copyright(c) 2015 - 2016 Muhammad Dadu
  * MIT Licensed
  */
-const
-	debug = require('debug')('clout:hook/middleware'),
-	sequelize = require('sequelize');
+const debug = require('debug')('clout:hook/middleware');
+const Sequelize = require('sequelize');
 
 module.exports = {
 	sequelize: {
 		event: 'start',
 		priority: 'MODEL',
 		fn: function (next) {
-			var conf = this.config.sequelize;
-			this.Sequelize = sequelize;
+			let conf = this.config.sequelize;
+			this.Sequelize = Sequelize;
+
 			if (conf.database === '<DATABASE>') {
-				debug('Configuration for clout-sequelize missing')
 				this.logger.warn('Configuration for clout-sequelize missing');
 				return next();
 			}
+
 			this.sequelize = new Sequelize(conf.database, conf.username, conf.password, conf.connection);
-			debug('sequelize initialized');
+			this.logger.info('sequelize initialized');
 			next();
 		}
 	},
@@ -28,20 +28,22 @@ module.exports = {
 		event: 'start',
 		priority: 20,
 		fn: function (next) {
-			var conf = this.config.sequelize,
-				sequelize = this.sequelize;
+			let conf = this.config.sequelize;
+			let sequelize = this.sequelize;
+
 			if (!conf.sync || !sequelize) {
-				debug('skipping model syncronize');
+				this.logger.info('skipping model syncronize');
 				return next();
 			}
+
 			// syncronize sequelize
-			debug('syncronizing sequelize');
+			this.logger.info('syncronizing sequelize');
 			sequelize.sync(conf.sync)
 				.then(function () {
-					debug('models synced');
+					this.logger.info('models synced');
 					next();
 				}, function (err) {
-					debug('sync error:', err);
+					this.logger.error('sync error:', err);
 					next(err);
 				});
 		}
